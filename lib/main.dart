d6a5f6663ce97248463ed7b4a5dac2a67fb5b749
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-12-31 12:24:28 +0530

    Added Changes

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,8 @@ class App extends StatelessWidget {
       theme: ThemeData().copyWith(
         //useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 63, 17, 177)),
+          seedColor: const Color.fromARGB(255, 63, 17, 177),
+        ),
       ),
       debugShowCheckedModeBanner: false,
       home: const AuthScreen(),
